0b9e4b03a9d804c572b7c9aae5fd029046aaf6e0
sreesanjay s <104411099+Sreesanjay@users.noreply.github.com>
2023-10-22 14:44:15 +0530

    Create BinarySearch.js
    binary-search

---
+++ BinarySearch.js
@@ -0,0 +1,19 @@
+let binarySearch=(arr,key)=>{
+    let left=0;
+    let right=arr.length-1;
+    while(left<=right){
+        let middle=Math.floor((right+left)/2)
+        console.log(left,right,middle)
+        if(arr[middle]==key) {
+            return middle+1;
+        }
+        if(key<arr[middle]){
+            right=middle-1;
+        }
+        else{
+            left=middle+1
+        }
+    }
+    return false
+}
+console.log(binarySearch([1,2,3,4,5,6,7,10],10));
