6816b65a24412e2bd30e6b1a0809bfe0af7bc74d
Sreesanjay <sreesanjay7592@gmail.com>
2023-11-02 23:00:46 +0530

    insertion sort updated

--- Sorting/insertion_sort.js
+++ Sorting/insertion_sort.js
@@ -5,7 +5,7 @@
  * @param {number} a 
  * @param {number} b 
  */
-const swap = (arr,a,b) => {
+const swap = (arr, a, b) => {
     let temp = arr[b];
     arr[b] = arr[a];
     arr[a] = temp;
@@ -17,17 +17,15 @@ const swap = (arr,a,b) => {
  * @returns {number[]}
  */
 const insertionSort = (arr) => {
-    for(let i = 1; i < arr.length; i++){
-        let j = i ;
-        while(j > 0){
-            if(arr[j] < arr[j-1]){
-                 swap(arr,j,j-1);
-                j--;
-            }else{
-                break;
-            }
-        }        
+    for (let i = 1; i < arr.length; i++) {
+        let j = i - 1;
+        let key = arr[i];
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j+1] = key;
     }
     return arr;
 }
-console.log(insertionSort([5,4,2,6,10,2]))
+console.log(insertionSort([5, 4, 2, 6, 10, 2]))
